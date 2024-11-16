2b2ad706878a08829d370dd9430153cb22a87bd1
spartan-triductran <tri.duc.tran@c0x12c.com>
2024-11-16 17:03:20 +0700

    Implement header

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import App from "./App";
 import "./index.css";
-
-const App = () => <h1>Hello, Voice Cloning!</h1>;
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
